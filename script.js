cf53214aa716ac76dc4b48f52f4e15be822e23ef
T-Donvulo <donvutha811@gmail.com>
2020-07-24 15:10:22 +0200

    greet updates

--- script.js
+++ script.js
@@ -1,33 +1,52 @@
 const nameEnter = document.querySelector(".nameEnter");
-const language = document.querySelector(".language");
+const mylanguages = document.querySelector(".languages");
 const addGreetingsBtn = document.querySelector(".greetings");
 const message = document.querySelector(".message");
 
 
-const greeted = [];
+ //const greet = greetings();
 
 addGreetingsBtn.addEventListener("click", function(){
-    if(nameEnter.value == ""){
-        message.innerHTML= "Enter your name please";
-        return;
-    }
+    //var removeName = nameEnter.value;
+    //removeName.innerHTML = ""; 
+    //alert("Donvulo")
+    var checkedRadioBtn = document.querySelector("input[name='language']:checked"); 
+    if (checkedRadioBtn){ 
+    var language = checkedRadioBtn.value 
+}
+//alert(language)
+if (language === "Swahili"){
+    message.innerHTML = "Hujambo "+ nameEnter.value; 
 
-    const greets = greetings.value.toUpperCase();
-    {
-            message.innerHTML = "";
-        }
-     if (!namesEnter.includes(greets)) {
-         const greetElem = document.createElement("div");
-         greetElem.classList.add("greet");
-         greetElem.innerHTML = greets;
-         greeting.appendChild(greetElem);
+}
+else if(language === "Tshivenda"){
+    message.innerHTML = "Aa "+ nameEnter.value;
+}
+else if(language === "Shona"){
+    message.innerHTML = "Mhoro " + nameEnter.value;
+}
 
-         greetingEntered.push(greets)
-         
-     }
-
-       
         
+
+
+    // if(nameEnter.value == ""){
+    //     message.innerHTML= "Enter your name please";
+    //     return;
+    // }    
+    // message.innerHTML = "choose your home language slima!";
+
+    // var checkedGreetingsBtn = document.querySelector("input[name='language']:checked");
     
+         
+        
+    //  if (!namesEnter.includes(greets)) {
+    //      const greetElem = document.createElement("div");
+    //      greetElem.classList.add("greet");
+    //      greetElem.innerHTML = greets;
+    //     //  greeting.appendChild(greetElem);
+
+    //      greetingEntered.push(greets)
+         
+     //}
 
 });
